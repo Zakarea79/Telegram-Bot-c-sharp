ed61935d13c98c0603a7d49ff21d6cbf01235dae
Zakarea79 <Zakarea6688@gmail.com>
2022-07-07 09:14:20 -0700

    Initial commit

---
+++ Telegram Bot/JsonData.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections.Generic;
+
+namespace Telegram_Bot
+{
+    #region inline keyborad
+    public class Inline_keyboardItemItem
+    {
+        public string text { get; set; }
+        public string callback_data { get; set; }
+    }
+    public class Inline_keyboard
+    {
+        public List<List<Inline_keyboardItemItem>> inline_keyboard { get; set; }
+    }
+
+    namespace InlineKeyBoradUrl 
+    {
+        public class Inline_keyboardItemItem
+        {
+            public string text { get; set; }
+            public string url { get; set; }
+        }
+        public class Inline_keyboard
+        {
+            public List<List<Inline_keyboardItemItem>> inline_keyboard { get; set; }
+        }
+    }
+
+    #endregion
+
+    #region miankeyBord
+    public class KeyboardItemItem
+    {
+        public string text { get; set; }
+    }
+
+    public class keyBord
+    {
+        public List<List<KeyboardItemItem>> keyboard { get; set; }
+        public bool one_time_keyboard { get; set; }
+        public bool resize_keyboard { get; set; }
+    }
+    #endregion
+
+    #region File Info Upload File
+    enum TypeMessage : byte
+    {
+        photo, voice,
+        document, animation,
+        sticker, audio
+    }
+    class FileUploadInfo
+    {
+        public string Token { get; set; }
+        public string PathFile { set; get; }
+        public int Chat_Id { set; get; }
+    }
+    #endregion
+
+    #region File Info Get
+    public class Result_Bot
+    {
+        public string file_id { get; set; }
+        public string file_unique_id { get; set; }
+        public int file_size { get; set; }
+        public string file_path { get; set; }
+    }
+    public class File_Bot
+    {
+        public bool ok { get; set; }
+        public Result_Bot result { get; set; }
+    }
+    #endregion
+
+    #region Get Update Message
+
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Animation
+    {
+        public string mime_type { get; set; }
+        public int duration { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public Thumb thumb { get; set; }
+        public string file_id { get; set; }
+        public string file_unique_id { get; set; }
+        public int file_size { get; set; }
+    }
+
+    public class Audio
+    {
+        public int duration { get; set; }
+        public string file_name { get; set; }
+        public string mime_type { get; set; }
+        public string title { get; set; }
+        public string performer { get; set; }
+        public string file_id { get; set; }
+        public string file_unique_id { get; set; }
+        public int file_size { get; set; }
+    }
+
+    public class CallbackQuery
+    {
+        public string id { get; set; }
+        public From from { get; set; }
+        public Message message { get; set; }
+        public string chat_instance { get; set; }
+        public string data { get; set; }
+    }
+
+    public class Chat
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string username { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Contact
+    {
+        public string phone_number { get; set; }
+        public string first_name { get; set; }
+        public string vcard { get; set; }
+    }
+
+    public class Document
+    {
+        public string mime_type { get; set; }
+        public Thumb thumb { get; set; }
+        public string file_id { get; set; }
+        public string file_unique_id { get; set; }
+        public int file_size { get; set; }
+        public string file_name { get; set; }
+    }
+
+    public class From
+    {
+        public decimal id { get; set; }
+        public bool is_bot { get; set; }
+        public string first_name { get; set; }
+        public string username { get; set; }
+        public string language_code { get; set; }
+    }
+
+    public class Location
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+
+    public class Message
+    {
+        public int message_id { get; set; }
+        public From from { get; set; }
+        public Chat chat { get; set; }
+        public int date { get; set; }
+        public string text { get; set; }
+        public ReplyMarkup reply_markup { get; set; }
+        public Animation animation { get; set; }
+        public Document document { get; set; }
+        public Sticker sticker { get; set; }
+        public Voice voice { get; set; }
+        public List<Photo> photo { get; set; }
+        public Contact contact { get; set; }
+        public Location location { get; set; }
+        public Audio audio { get; set; }
+    }
+
+    public class Photo
+    {
+        public string file_id { get; set; }
+        public string file_unique_id { get; set; }
+        public int file_size { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+
+    public class ReplyMarkup
+    {
+        public List<List<Inline_keyboardItemItem>> inline_keyboard { get; set; }
+    }
+
+    public class Result
+    {
+        public int update_id { get; set; }
+        public CallbackQuery callback_query { get; set; }
+        public Message message { get; set; }
+    }
+
+    public class Messase_Bot
+    {
+        public bool ok { get; set; }
+        public List<Result> result { get; set; }
+    }
+
+    public class Sticker
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public string emoji { get; set; }
+        public string set_name { get; set; }
+        public bool is_animated { get; set; }
+        public bool is_video { get; set; }
+        public Thumb thumb { get; set; }
+        public string file_id { get; set; }
+        public string file_unique_id { get; set; }
+        public int file_size { get; set; }
+    }
+
+    public class Thumb
+    {
+        public string file_id { get; set; }
+        public string file_unique_id { get; set; }
+        public int file_size { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+
+    public class Voice
+    {
+        public int duration { get; set; }
+        public string mime_type { get; set; }
+        public string file_id { get; set; }
+        public string file_unique_id { get; set; }
+        public int file_size { get; set; }
+    }
+
+
+
+
+
+    #endregion
+}
---
+++ Telegram Bot/Program.cs
@@ -0,0 +1,155 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using static System.Console;
+
+namespace Telegram_Bot
+{
+    class Program
+    {
+        static WebClient Client = new WebClient();
+        static string Token = "5244510127:AAGlfa79LXntpQfrPk3_lxQUj8syi1xefy0";
+        public static void Main(string[] args)
+        {
+            long ofset = 0;
+            string url = $"https://api.telegram.org/bot{Token}/";
+
+            while (true)
+            {
+                try
+                {
+                    var response = JsonConvert.DeserializeObject<Messase_Bot>(Client.DownloadString(url + $"getUpdates?offset={ofset}"));
+                    foreach (var item in response.result)
+                    {
+                        WriteLine("Message Update : " + item.update_id);
+
+                        if (item.callback_query != null)
+                        {
+                            Client.DownloadString($"{url}sendMessage?chat_id={item.callback_query.message.chat.id}&text=Hello");
+                        }
+                        else if (item.message.text == "/start")
+                        {
+                            _ = Client.DownloadString($"{url}sendMessage?chat_id={item.message.chat.id}&text={item.message.text}");
+                        }
+                        else if (item.message.text == "key")
+                        {
+                            //var ananimousClass = new { inline_keyboard = new Dictionary<string, string>() { "hi" } };
+                            //string json = "{\"inline_keyboard\": [[{\"text\": \"hi\",\"callback_data\": \"hi\"} , {\"text\": \"hi\",\"callback_data\": \"hi\"}] , [{\"text\": \"hi2\",\"callback_data\": \"hi2\"}]]}";
+                            var a = new Inline_keyboard();
+                            a.inline_keyboard = new List<List<Inline_keyboardItemItem>>();
+                            a.inline_keyboard.Add(new List<Inline_keyboardItemItem>
+                            {
+                                new Inline_keyboardItemItem(){text = "Hello" , callback_data = "htt"} ,
+                                new Inline_keyboardItemItem(){text = "Hi" , callback_data = "key2"} ,
+                                new Inline_keyboardItemItem(){text = "ok" , callback_data = "key3"}
+                            });
+                            a.inline_keyboard.Add(new List<Inline_keyboardItemItem>
+                            {
+                                new Inline_keyboardItemItem(){text = "Hello" , callback_data = "key1"}
+                            });
+                            string jesonV = JsonConvert.SerializeObject(a);
+                            Client.DownloadString($"{url}sendMessage?chat_id={item.message.chat.id}&text={item.message.text}&reply_markup={jesonV}");
+                        }
+                        else if (item.message.text == "key3") 
+                        {
+                            var a = new InlineKeyBoradUrl.Inline_keyboard();
+                            a.inline_keyboard = new List<List<InlineKeyBoradUrl.Inline_keyboardItemItem>>();
+                            a.inline_keyboard.Add(new List<InlineKeyBoradUrl.Inline_keyboardItemItem>() 
+                            {
+                                new InlineKeyBoradUrl.Inline_keyboardItemItem(){text = "Hello" ,url = "http://google.com" }
+                            });
+                            Client.DownloadString($"{url}sendMessage?chat_id={item.message.chat.id}&text={item.message.text}&reply_markup={JsonConvert.SerializeObject(a)}");
+                        }
+                        else if(item.message.text == "key2") 
+                        {
+                            var a = new keyBord();
+                            a.keyboard = new List<List<KeyboardItemItem>>();
+                            a.keyboard.Add(new List<KeyboardItemItem> 
+                            {
+                                new KeyboardItemItem(){text = "ky1"} , new KeyboardItemItem(){text = "key2"}
+                            });
+                            a.keyboard.Add(new List<KeyboardItemItem>
+                            {
+                                new KeyboardItemItem(){text = "ky1"}
+                            });
+                            a.one_time_keyboard = true;
+                            a.resize_keyboard = true;
+                            Client.DownloadString($"{url}sendMessage?chat_id={item.message.chat.id}&text={item.message.text}&reply_markup={JsonConvert.SerializeObject(a)}");
+                        }
+                        else if (item.message.photo != null)
+                        {
+                            for (int i = 0; i < item.message.photo.Count; i++)
+                            {
+                                var File = JsonConvert.DeserializeObject<File_Bot>(Client.DownloadString($"https://api.telegram.org/bot{Token}/getFile?file_id={item.message.photo[i].file_id}"));
+                                Client.DownloadFile($"https://api.telegram.org/file/bot{Token}/{File.result.file_path}", $"d:\\DirPhoto\\{Path.GetFileName(i + File.result.file_path)}");
+                            }
+                        }
+                        else if (item.message.document != null)
+                        {
+                            var File = JsonConvert.DeserializeObject<File_Bot>(Client.DownloadString($"https://api.telegram.org/bot{Token}/getFile?file_id={item.message.document.file_id}"));
+                            Client.DownloadFile($"https://api.telegram.org/file/bot{Token}/{File.result.file_path}", $"d:\\DirPhoto\\{Path.GetFileName(File.result.file_path)}");
+                        }
+                        else if (item.message.voice != null)
+                        {
+                            var File = JsonConvert.DeserializeObject<File_Bot>(Client.DownloadString($"https://api.telegram.org/bot{Token}/getFile?file_id={item.message.voice.file_id}"));
+                            Client.DownloadFile($"https://api.telegram.org/file/bot{Token}/{File.result.file_path}", $"d:\\DirPhoto\\{Path.GetFileName(File.result.file_path)}");
+                        }
+                        else if (item.message.sticker != null)
+                        {
+                            WriteLine(Upload(new FileUploadInfo
+                            {
+                                Token = Token,
+                                Chat_Id = item.message.chat.id,
+                                PathFile = @"C:\Users\SZA\Desktop\Untitled-1.jpg"
+                            }, TypeMessage.sticker));
+                        }
+                        else if (item.message.contact != null)
+                        {
+                            _ = Client.DownloadString($"{url}sendContact?chat_id={item.message.chat.id}&phone_number=12345&first_name=reza");
+                        }
+                        else if (item.message.text == "photo")
+                        {
+                            //_ = Client.DownloadString($"https://api.telegram.org/bot{Token}/sendSticker?chat_id={item.message.chat.id}&sticker=CAADAgADOQADfyesDlKEqOOd72VKAg");
+
+                            WriteLine(Upload(new FileUploadInfo
+                            {
+                                Token = Token,
+                                Chat_Id = item.message.chat.id,
+                                PathFile = @"C:\Users\SZA\Desktop\Untitled-1.jpg"
+                            }, TypeMessage.photo));
+                        }
+                        else 
+                        {
+                            Client.DownloadString($"{url}sendMessage?chat_id={item.message.chat.id}&text={item.message.text}");
+                        }
+                        ofset = item.update_id + 1;
+                        WriteLine("----------------");
+                    }
+                    System.Threading.Thread.Sleep(10);
+                }
+                catch (System.Exception ex)
+                {
+                    WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public static string Upload(FileUploadInfo fileInfo, TypeMessage type)
+        {
+            string url = $"https://api.telegram.org/bot{fileInfo.Token}/send{type}?chat_id={fileInfo.Chat_Id}";
+            using (var client = new HttpClient())
+            {
+                using (var content = new MultipartFormDataContent())
+                {
+                    content.Add(new StreamContent(new MemoryStream(File.ReadAllBytes(fileInfo.PathFile))), type.ToString(), Path.GetFileName(fileInfo.PathFile));
+                    using (var message = client.PostAsync(url, content).Result)
+                    {
+                        return message.Content.ReadAsStringAsync().Result;
+                    }
+                }
+            }
+        }
+    }
+}
